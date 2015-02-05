b45712e14b4c4be7c4ef07868c6ce2c36dba7133
Chris Mytton <chrismytton@gmail.com>
2015-02-05 23:57:40 +0000

    Add original pitchfork scraper

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,37 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'mechanize'
+require 'date'
+require 'json'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+agent = Mechanize.new
+page = agent.get("http://pitchfork.com/reviews/albums/")
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+review_links = page.links_with(href: %r{^/reviews/albums/\w+})
+
+review_links = review_links.reject do |link|
+  parent_classes = link.node.parent['class'].split
+  parent_classes.any? { |p| %w[next-container page-number].include?(p) }
+end
+
+review_links = review_links[0...4]
+
+reviews = review_links.map do |link|
+  review = link.click
+  review_meta = review.search('#main .review-meta .info')
+  artist = review_meta.search('h1')[0].text
+  album = review_meta.search('h2')[0].text
+  label, year = review_meta.search('h3')[0].text.split(';').map(&:strip)
+  reviewer = review_meta.search('h4 address')[0].text
+  review_date = Date.parse(review_meta.search('.pub-date')[0].text)
+  score = review_meta.search('.score').text.to_f
+  {
+    artist: artist,
+    album: album,
+    label: label,
+    year: year,
+    reviewer: reviewer,
+    review_date: review_date,
+    score: score
+  }
+end
+
+puts JSON.pretty_generate(reviews)
