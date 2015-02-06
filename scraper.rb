1e942922d85c335cd8d652b16177c90892478a03
Chris Mytton <chrismytton@gmail.com>
2015-02-06 00:10:35 +0000

    Update scraper to using scraperwiki gem
    This persists the results of the scraper to a sqlite database for later
    retrieval.

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,8 @@
 require 'mechanize'
 require 'date'
-require 'json'
+require 'scraperwiki'
+
+ScraperWiki.config = { db: 'data.sqlite', default_table_name: 'data' }
 
 agent = Mechanize.new
 page = agent.get("http://pitchfork.com/reviews/albums/")
@@ -34,4 +36,6 @@ reviews = review_links.map do |link|
   }
 end
 
-puts JSON.pretty_generate(reviews)
+reviews.each do |review|
+  ScraperWiki.save_sqlite([:artist, :album], review)
+end
